3ff0f6281d8101177098f0b702f658595ca41b7a
princethakur19 <princethakur7778@gmail.com>
2026-07-18 23:14:49 +0530

    push commit

--- frontend/src/admin/components/AdminNavbar.jsx
+++ frontend/src/admin/components/AdminNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useStae } from 'react';
+import React, { useState } from 'react';
 
 const AdminNavbar = ({ activeSection, onSectionChange, onLogout }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);

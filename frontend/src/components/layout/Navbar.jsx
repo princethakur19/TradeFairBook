b37d6b3e000e2f2d3dd3f87ed23a51f5890a083d
princethakur19 <princethakur7778@gmail.com>
2026-07-21 22:26:33 +0530

    push commit

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -155,34 +155,49 @@ const Navbar = () => {
             >
               <svg viewBox="0 0 69.667 44" aria-hidden="true" focusable="false">
                 <g transform="translate(3.5 3.5)">
-                  <rect className="theme-toggle-track" rx="17.5" height="35" width="60.667" />
-                  <g className="theme-toggle-thumb" transform="translate(2.333 2.333)">
-                    <g className="theme-toggle-sun">
-                      <circle className="theme-toggle-sun-outer" r="15.167" cy="15.167" cx="15.167" />
-                      <circle className="theme-toggle-sun-glow" r="11.667" cy="15.167" cx="15.167" />
-                      <circle className="theme-toggle-sun-inner" r="7" cy="15.167" cx="15.167" />
+                  <g transform="matrix(1, 0, 0, 1, -3.5, -3.5)">
+                    <rect className="theme-switch-container" transform="translate(3.5 3.5)" rx="17.5" height="35" width="60.667" />
+                  </g>
+
+                  <g className="theme-switch-button" transform="translate(2.333 2.333)">
+                    <g className="theme-switch-sun">
+                      <g transform="matrix(1, 0, 0, 1, -5.83, -5.83)">
+                        <circle className="theme-switch-sun-outer" transform="translate(5.83 5.83)" r="15.167" cy="15.167" cx="15.167" />
+                      </g>
+                      <g transform="matrix(1, 0, 0, 1, -5.83, -5.83)">
+                        <path className="theme-switch-sun-glow" transform="translate(9.33 9.33)" d="M11.667,0A11.667,11.667,0,1,1,0,11.667,11.667,11.667,0,0,1,11.667,0Z" />
+                      </g>
+                      <circle className="theme-switch-sun-inner" transform="translate(8.167 8.167)" r="7" cy="7" cx="7" />
                     </g>
-                    <g className="theme-toggle-moon" transform="translate(25.667 0)">
-                      <circle className="theme-toggle-moon-body" r="15.167" cy="15.167" cx="15.167" />
-                      <g className="theme-toggle-moon-patches">
-                        <circle r="2" cy="6.5" cx="18.6" />
-                        <circle r="2" cy="20" cx="15" />
-                        <circle r="1" cy="10" cx="8.7" />
-                        <circle r="1" cy="20.9" cx="26.8" />
-                        <circle r="1.5" cy="12.5" cx="25.8" />
+
+                    <g className="theme-switch-moon">
+                      <g transform="matrix(1, 0, 0, 1, -31.5, -5.83)">
+                        <circle className="theme-switch-moon-body" transform="translate(31.5 5.83)" r="15.167" cy="15.167" cx="15.167" />
+                      </g>
+                      <g className="theme-switch-patches" transform="translate(-24.415 -1.009)">
+                        <circle transform="translate(43.009 4.496)" r="2" cy="2" cx="2" />
+                        <circle transform="translate(39.366 17.952)" r="2" cy="2" cx="2" />
+                        <circle transform="translate(33.016 8.044)" r="1" cy="1" cx="1" />
+                        <circle transform="translate(51.081 18.888)" r="1" cy="1" cx="1" />
+                        <circle transform="translate(33.016 22.503)" r="1" cy="1" cx="1" />
+                        <circle transform="translate(50.081 10.53)" r="1.5" cy="1.5" cx="1.5" />
                       </g>
                     </g>
                   </g>
-                  <path
-                    className="theme-toggle-cloud"
-                    d="M46.34,12.88c1.1,0,2.12,.29,3.01,.8,.06-.86,.73-1.55,1.58-1.65,1.22-.14,2.32,.12,3.26,.68,.02-1.14,.4-2.06,1.13-2.68,.9-.76,2.24-.86,3.25-.24,.91,.56,1.33,1.58,1.12,2.62,1.85,.03,3.25,.7,3.85,1.84,.68,1.3,.06,2.47-.62,3.18,.83,.22,1.58,.55,2.23,.98,.58,.38,.86,1.02,.75,1.72-.18,1.14-1.44,2.05-3.46,2.5-1.32,.3-2.79,.18-4.12-.34-.83-.33-1.42-1.01-1.75-1.8-.75,.76-1.62,1.25-2.53,1.25-1.1,0-2.06-.49-2.84-1.15-.92,.55-1.98,.76-3.16,.6-1.64-.22-2.77-.94-3.26-2.08-.36-.84-.27-1.74,.02-2.46-1.4-.53-2.35-1.57-2.35-2.78,0-1.64,1.75-2.97,3.89-2.97Z"
-                  />
-                  <g className="theme-toggle-stars">
-                    <path d="M15.2 5.1l.9 1.8 2 .3-1.5 1.4 .4 2-1.8-1-1.8 1 .4-2-1.5-1.4 2-.3z" />
-                    <path d="M24.5 13.8l.6 1.1 1.2.2-.9.8 .2 1.2-1.1-.6-1.1.6 .2-1.2-.9-.8 1.2-.2z" />
-                    <path d="M10.1 23.5l.7 1.4 1.6.2-1.2 1.1 .3 1.6-1.4-.8-1.4.8 .3-1.6-1.2-1.1 1.6-.2z" />
-                    <path d="M27.4 28.6l.5 1 .1 1.1-1-.5-1 .5 .2-1.1-.8-.8 1.1-.2z" />
-                    <path d="M7.5 10.8l.5 1 .1 1.1-1-.5-1 .5 .2-1.1-.8-.8 1.1-.2z" />
+
+                  <g transform="matrix(1, 0, 0, 1, -3.5, -3.5)">
+                    <path className="theme-switch-cloud" transform="translate(-3466.47 -160.94)" d="M3512.81,173.815a4.463,4.463,0,0,1,2.243.62.95.95,0,0,1,.72-1.281,4.852,4.852,0,0,1,2.623.519c.034.02-.5-1.968.281-2.716a2.117,2.117,0,0,1,2.829-.274,1.821,1.821,0,0,1,.854,1.858c.063.037,2.594-.049,3.285,1.273s-.865,2.544-.807,2.626a12.192,12.192,0,0,1,2.278.892c.553.448,1.106,1.992-1.62,2.927a7.742,7.742,0,0,1-3.762-.3c-1.28-.49-1.181-2.65-1.137-2.624s-1.417,2.2-2.623,2.2a4.172,4.172,0,0,1-2.394-1.206,3.825,3.825,0,0,1-2.771.774c-3.429-.46-2.333-3.267-2.2-3.55A3.721,3.721,0,0,1,3512.81,173.815Z" />
+                  </g>
+
+                  <g className="theme-switch-stars" transform="translate(3.585 1.325)">
+                    <path transform="matrix(-1, 0.017, -0.017, -1, 24.231, 3.055)" d="M.774,0,.566.559,0,.539.458.933.25,1.492l.485-.361.458.394L1.024.953,1.509.592.943.572Z" />
+                    <path transform="matrix(-0.777, 0.629, -0.629, -0.777, 23.185, 12.358)" d="M1.341.529.836.472.736,0,.505.46,0,.4.4.729l-.231.46L.605.932l.4.326L.9.786Z" />
+                    <path transform="matrix(0.438, 0.899, -0.899, 0.438, 23.177, 29.735)" d="M.015,1.065.475.9l.285.365L.766.772l.46-.164L.745.494.751,0,.481.407,0,.293.285.658Z" />
+                    <path transform="translate(12.677 0.388) rotate(104)" d="M1.161,1.6,1.059,1,1.574.722.962.607.86,0,.613.572,0,.457.446.881.2,1.454l.516-.274Z" />
+                    <path transform="matrix(-0.07, 0.998, -0.998, -0.07, 11.066, 15.457)" d="M.873,1.648l.114-.62L1.579.945,1.03.62,1.144,0,.706.464.157.139.438.7,0,1.167l.592-.083Z" />
+                    <path transform="translate(8.326 28.061) rotate(11)" d="M.593,0,.638.724,0,.982l.7.211.045.724.36-.64.7.211L1.342.935,1.7.294,1.063.552Z" />
+                    <path transform="translate(5.012 5.962) rotate(172)" d="M.816,0,.5.455,0,.311.323.767l-.312.455.516-.215.323.456L.827.911,1.343.7.839.552Z" />
+                    <path transform="translate(2.218 14.616) rotate(169)" d="M1.261,0,.774.571.114.3.487.967,0,1.538.728,1.32l.372.662.047-.749.728-.218L1.215.749Z" />
                   </g>
                 </g>
               </svg>

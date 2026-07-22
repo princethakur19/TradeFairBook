df3ebe0b071be2e2e2b1bd9e143bcc74b275a9a9
princethakur19 <princethakur7778@gmail.com>
2026-07-22 23:11:54 +0530

    correct error

--- frontend/src/admin/components/AdminNavbar.jsx
+++ frontend/src/admin/components/AdminNavbar.jsx
@@ -1,4 +1,4 @@
-import React, {  } from 'react';
+import React, { useState } from 'react';
 
 const AdminNavbar = ({ activeSection, onSectionChange, onLogout }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
